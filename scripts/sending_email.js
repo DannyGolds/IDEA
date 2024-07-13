9b57713fb27ac573d97e20a7db45d41048f98f0a
DannyGolds <danchik23122004@gmail.com>
2024-07-13 20:49:25 +0300

    regex commited

--- scripts/sending_email.js
+++ scripts/sending_email.js
@@ -1,35 +1,42 @@
 const serviceID = "service_5jhztzi"
 const templateID = "template_ldp58ue"
+const mailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+const phoneRegex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+
+function clearForm() {
+  Array.from(document.getElementById("postForm")).forEach((i) => {
+    console.log(i.value)
+    if (i.tagName == "INPUT") i.value = ""
+  })
+}
 
 fBtn.onclick = (ev) => {
-  let templateParams = {
-    from_name: document.getElementById("name").value,
-    from_tel: document.getElementById("number").value,
-    from_mail: document.getElementById("mail").value,
-  }
   ev.preventDefault()
-  emailjs
-    .send(serviceID, templateID, templateParams)
-    .then((result, error) => {
-      console.log(result)
-      document.getElementById("name").value = ""
-      document.getElementById("number").value = ""
-      document.getElementById("mail").value = ""
-      if (result.text == "OK") {
-        notify.innerHTML = "<p>✓</p>"
-        notify.classList.add("access")
-        setTimeout(() => {
-          notify.classList.remove("access")
-        }, 5000)
-      } else {
-        notify.innerHTML = "<p>x</p>"
-        notify.classList.add("error")
-        setTimeout(() => {
-          notify.classList.remove("error")
-        }, 5000)
-      }
-    })
-    .catch((error) => {
-      alert("К сожаления произошла ошибка!" + error.name)
-    })
+  if (
+    Array.from(document.getElementById("postForm"))[1].value.match(
+      phoneRegex
+    ) &&
+    Array.from(document.getElementById("postForm"))[2].value.match(mailRegex)
+  ) {
+    emailjs
+      .sendForm(serviceID, templateID, document.getElementById("postForm"))
+      .then((response) => {
+        if (response.text == "OK") {
+          notify.innerHTML = "<p>✓</p>"
+          notify.classList.add("access")
+          setTimeout(() => {
+            notify.classList.remove("access")
+          }, 5000)
+        } else {
+          notify.innerHTML = "<p>x</p>"
+          notify.classList.add("error")
+          setTimeout(() => {
+            notify.classList.remove("error")
+          }, 5000)
+        }
+        clearForm()
+      })
+  } else {
+    alert(new Error("Введите корректные данные!"))
+  }
 }
