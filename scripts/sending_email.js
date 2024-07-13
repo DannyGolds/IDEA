bab5d264ab1267bfa6ef32c53a074c6083400eca
DannyGolds <danchik23122004@gmail.com>
2024-07-13 10:27:15 +0300

    add notify

--- scripts/sending_email.js
+++ scripts/sending_email.js
@@ -8,8 +8,28 @@ fBtn.onclick = (ev) => {
     from_mail: document.getElementById("mail").value,
   }
   ev.preventDefault()
-  emailjs.send(serviceID, templateID, templateParams).then((result, error) => {
-    result ? console.log("Wow, ", result) : console.log("Bang, ", error)
-    console.log(templateParams)
-  })
+  emailjs
+    .send(serviceID, templateID, templateParams)
+    .then((result, error) => {
+      console.log(result)
+      document.getElementById("name").value = ""
+      document.getElementById("number").value = ""
+      document.getElementById("mail").value = ""
+      if (result.text == "OK") {
+        notify.innerHTML = "<p>✓</p>"
+        notify.classList.add("access")
+        setTimeout(() => {
+          notify.classList.remove("access")
+        }, 5000)
+      } else {
+        notify.innerHTML = "<p>x</p>"
+        notify.classList.add("error")
+        setTimeout(() => {
+          notify.classList.remove("error")
+        }, 5000)
+      }
+    })
+    .catch((error) => {
+      alert("К сожаления произошла ошибка!" + error.name)
+    })
 }
